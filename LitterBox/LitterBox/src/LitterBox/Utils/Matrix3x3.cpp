d7c57e2b14e66f3a31fc6a8fca680df9f9c1e848
Hizzykid <112919588+B0B0ChaCha@users.noreply.github.com>
2023-09-22 23:46:46 +0800

    Vector has been updated
    Changed math to math class template

--- LitterBox/LitterBox/src/LitterBox/Utils/Matrix3x3.cpp
+++ LitterBox/LitterBox/src/LitterBox/Utils/Matrix3x3.cpp
@@ -11,23 +11,23 @@ namespace LBMatrix
 	//	m20 = 0.0f; m21 = 0.0f; m22 = 0.0f;
 	//}
 
-	Matrix3x3::Matrix3x3()
-	{
-		//matrix.m00 = 0.0f; matrix.m01 = 0.0f; matrix.m02 = 0.0f;
-		//matrix.m10 = 0.0f; matrix.m11 = 0.0f; matrix.m12 = 0.0f;
-		//matrix.m20 = 0.0f; matrix.m21 = 0.0f; matrix.m22 = 0.0f;
-	}
+	//Matrix3x3::Matrix3x3()
+	//{
+	//	//matrix.m00 = 0.0f; matrix.m01 = 0.0f; matrix.m02 = 0.0f;
+	//	//matrix.m10 = 0.0f; matrix.m11 = 0.0f; matrix.m12 = 0.0f;
+	//	//matrix.m20 = 0.0f; matrix.m21 = 0.0f; matrix.m22 = 0.0f;
+	//}
 
-	Matrix3x3::~Matrix3x3() {}
+	//Matrix3x3::~Matrix3x3() {}
 
-	Matrix3x3::Matrix3x3(float a11, float a12, float a13, 
-						float a21, float a22, float a23, 
-						float a31, float a32, float a33)
-	{
-		vec[0] = { a11, a12, a13 };
-		vec[1] = { a21, a22, a23 };
-		vec[2] = { a31, a32, a33 };
-	}
+	//Matrix3x3::Matrix3x3(float a11, float a12, float a13, 
+	//					float a21, float a22, float a23, 
+	//					float a31, float a32, float a33)
+	//{
+	//	vec[0] = { a11, a12, a13 };
+	//	vec[1] = { a21, a22, a23 };
+	//	vec[2] = { a31, a32, a33 };
+	//}
 
 	//Matrix3x3& Matrix3x3::operator=(const Matrix3x3& rhs)
 	//{
