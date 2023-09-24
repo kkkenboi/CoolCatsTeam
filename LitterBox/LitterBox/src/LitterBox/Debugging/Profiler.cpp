36b5709948ee677da01d9fcfc1e3f1714703b975
JarrettAng <jarrett_ang@hotmail.com>
2023-09-24 13:12:49 +0800

    Profiler WIP 3

--- LitterBox/LitterBox/src/LitterBox/Debugging/Profiler.cpp
+++ LitterBox/LitterBox/src/LitterBox/Debugging/Profiler.cpp
@@ -14,24 +14,36 @@ namespace LB
 {
 	ProfilerManager* PROFILER = nullptr;
 
+	/**************************************************************************************************/
+	// Profiler manager
 	ProfilerManager::ProfilerManager() 
 	{
 		if (!PROFILER)
 			PROFILER = this;
 		else
 			std::cerr << "Profiling System already exist" << std::endl;
+
+		INPUT->SubscribeToKey(DumpInfo, dumpInfoKey, KeyEvent::TRIGGERED);
 	}
 
-	// Profiler object
-	Profiler::Profiler() 
+	void ProfilerManager::Destroy() 
 	{
-		start = TIME->GetTimeStamp();
+		INPUT->UnsubscribeToKey(DumpInfo, dumpInfoKey, KeyEvent::TRIGGERED);
 	}
+
+	void DumpInfo()
+	{
+
+	}
+
+	/**************************************************************************************************/
+	// Profiler object
+	Profiler::Profiler(char const* _name, ProfileResult _result) : name(_name), result(_result), start(TIME->GetTimeStamp()) { }
 
 	Profiler::~Profiler() 
 	{
 		std::chrono::high_resolution_clock::time_point end = TIME->GetTimeStamp();
-
+		std::chrono::duration<double> duration{end - start};
 	}
 
 }
--- LitterBox/LitterBox/src/LitterBox/Debugging/Profiler.h
+++ LitterBox/LitterBox/src/LitterBox/Debugging/Profiler.h
@@ -13,34 +13,48 @@
 #include <list>
 #include "LitterBox/Core/System.h"
 #include "LitterBox/Engine/Time.h"
+#include "LitterBox/Engine/Input.h"
 
 namespace LB 
 {
+	enum class ProfileResult {
+		IMMEDIATE,	// Prints the result immediately after profiling
+		MANAGER		// Adds the result to printing queue in the manager
+	};
+
 	struct ProfileInfo
 	{
-		std::string 
+		char const* name;
+		double duration;
 	};
 
 	class Profiler 
 	{
 		public:
-		Profiler();
+		Profiler(char const* name, ProfileResult result);
 		~Profiler();
 
 		private:
 		std::chrono::high_resolution_clock::time_point start;
+		char const* name;
+		ProfileResult result;
 	};
 
 	class ProfilerManager : public ISystem 
 	{
 		public:
+		virtual std::string GetName() { return "Profiling System"; }
+		
 		ProfilerManager();
 
-		virtual std::string GetName() { return "Profiling System"; }
+		virtual void Destroy() override;
 
 		private:
-		std::list<
+		KeyCode dumpInfoKey = KeyCode::KEY_P;
+		std::list<ProfileInfo> printQueue;
 	};
+
+	void DumpInfo();
 
 	extern ProfilerManager* PROFILER;
 }
