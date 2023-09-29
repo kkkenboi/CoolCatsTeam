8cdd575627ac8728e90e86b701ede031b787cddf
Hizzykid <112919588+B0B0ChaCha@users.noreply.github.com>
2023-09-29 11:16:12 +0800

    Updated Header for Math and Matrix

--- LitterBox/LitterBox/src/LitterBox/Utils/Math.h
+++ LitterBox/LitterBox/src/LitterBox/Utils/Math.h
@@ -11,6 +11,9 @@
  \brief
  The functions in the Math class include:
 
+ Copyright (C) 2023 DigiPen Institute of Technology. Reproduction or
+ disclosure of this file or its contents without the prior written consent
+ of DigiPen Institute of Technology is prohibited.
 **************************************************************************/
 
 #pragma once
