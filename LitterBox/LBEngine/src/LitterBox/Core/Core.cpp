d6bcdf653a07bd659b9c6659c25be8239a6e15ed
JarrettAng <jarrett_ang@hotmail.com>
2023-11-03 14:03:15 +0800

    Commenting Part (1)

--- LitterBox/LBEngine/src/LitterBox/Core/Core.cpp
+++ LitterBox/LBEngine/src/LitterBox/Core/Core.cpp
@@ -1,11 +1,10 @@
 /*!************************************************************************
  \file				Core.cpp
- \author(s)			Kenji Brannon Chong
- \par DP email(s):	kenjibrannon.c@digipen.edu
+ \author(s)			Kenji Brannon Chong, Ang Jiawei Jarrett
+ \par DP email(s):	kenjibrannon.c@digipen.edu, a.jiaweijarrett@digipen.edu
  \par Course:       CSD2401A
  \date				29/09/2023
  \brief
-
  This file contains functions defintions of the LBEngine class that controls
  the engine and systems. Has a layer system to allow for ImGui implementation
  later on.
@@ -25,6 +24,10 @@ namespace LB
 {
 	LBEngine* CORE;
 
+	/*!***********************************************************************
+	 \brief
+	 Sets the global pointer to this instance
+	*************************************************************************/
 	LBEngine::LBEngine()
 	{
 		if (!CORE)
@@ -36,9 +39,6 @@ namespace LB
 	/*!***********************************************************************
 	 \brief
 	 Initalises the Engine by running it and initialises all its systems too
-
-	 \return
-	 Nothing
 	*************************************************************************/
 	void LBEngine::Initialise()
 	{
@@ -51,9 +51,6 @@ namespace LB
 	/*!***********************************************************************
 	 \brief
 	 The game loop where all the layers, systems and time updates
-
-	 \return
-	 Nothing
 	*************************************************************************/
 	void LBEngine::GameLoop()
 	{
@@ -87,9 +84,6 @@ namespace LB
 	/*!***********************************************************************
 	 \brief
 	 Sends a message to all of the systems
-
-	 \return
-	 Nothing
 	*************************************************************************/
 	void LBEngine::BroadcastMessage(Message* message)
 	{
@@ -105,9 +99,6 @@ namespace LB
 	/*!***********************************************************************
 	 \brief
 	 Adds a system into the engine
-
-	 \return
-	 Nothing
 	*************************************************************************/
 	void LBEngine::AddSystem(std::shared_ptr<ISystem> system)
 	{
@@ -118,9 +109,6 @@ namespace LB
 	/*!***********************************************************************
 	 \brief
 	 Destroy all the systems attached to the systems in reverse order
-
-	 \return
-	 Nothing
 	*************************************************************************/
 	void LBEngine::DestroySystems()
 	{
@@ -136,53 +124,87 @@ namespace LB
 
 	/*!***********************************************************************
 	 \brief
-	 Checks if the engine is running
-
-	 \return
-	 True if running, else false
+	 Returns true if the engine is running
 	*************************************************************************/
 	bool LBEngine::IsRunning() const
 	{
 		return m_running;
 	}
 
+	/*!***********************************************************************
+	 \brief
+	 Returns true if the game is in editor mode, m_editorMode is a bool 
+	 to check if the game is in the editor view
+	*************************************************************************/
 	bool LBEngine::IsEditorMode() const
 	{
 		return m_editorMode;
 	}
 
+	/*!***********************************************************************
+	 \brief
+	 Flips the state of m_editorMode which is a bool to check if the game
+	 is in the editor view
+	*************************************************************************/
 	void LBEngine::ToggleEditorMode()
 	{
 		SetEditorMode(!m_editorMode);
 	}
 
+	/*!***********************************************************************
+	 \brief
+	 Sets the state of m_editorMode to the given state, m_editorMode is a bool 
+	 to check if the game is in the editor view
+	*************************************************************************/
 	void LBEngine::SetEditorMode(bool newState)
 	{
 		m_editorMode = newState;
 		onEditorModeToggle.Invoke(m_editorMode);
 	}
 
+	/*!***********************************************************************
+	 \brief
+	 Returns true if the actual game is playing
+	*************************************************************************/
 	bool LBEngine::IsPlaying() const
 	{
 		return m_isPlaying;
 	}
 
+	/*!***********************************************************************
+	 \brief
+	 Flips the m_isPlaying state
+	*************************************************************************/
 	void LBEngine::TogglePlaying()
 	{
 		SetPlayingMode(!m_isPlaying);
 	}
 
+	/*!***********************************************************************
+	 \brief
+	 Sets the m_isPlaying state to the given one, m_isPlaying checks if the
+	 game is actually running (not editor)
+	*************************************************************************/
 	void LBEngine::SetPlayingMode(bool newState)
 	{
 		m_isPlaying = newState;
 		onPlayingModeToggle.Invoke(m_isPlaying);
 	}
 
+	/*!***********************************************************************
+	 \brief
+	 Returns true if the editor ImGUI has initialized and launched
+	*************************************************************************/
 	bool LBEngine::IsEditorLaunched() const
 	{
 		return m_editorLaunch;
 	}
 
+	/*!***********************************************************************
+	 \brief
+	 Sets the m_editorLaunch flag to the given state, this checks if the
+	 editor ImGUI has initialized and launched
+	*************************************************************************/
 	void LBEngine::SetEditorLaunched(bool newState)
 	{
 		m_editorLaunch = newState;
