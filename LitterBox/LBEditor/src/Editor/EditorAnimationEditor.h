383aba147405b12384fb832c53314a4731883250
Hizzykid <112919588+B0B0ChaCha@users.noreply.github.com>
2023-11-27 11:40:24 +0800

    AnimationEditor Commented, Chaser, Player Commented

--- LitterBox/LBEditor/src/Editor/EditorAnimationEditor.h
+++ LitterBox/LBEditor/src/Editor/EditorAnimationEditor.h
@@ -1,3 +1,19 @@
+/*!************************************************************************
+ \file				EditorAnimationEditor.h
+ \author(s)			Vanessa Chua Siew Jin
+ \par DP email(s):	vanessasiewjin@digipen.edu
+ \par Course:		CSD2401A
+ \date				25-11-2023
+ \brief
+ This file contains the EditorAniamtionEditor class and all its functionalities
+ for the aniamtion view layer of the Editor.
+
+ Editor.
+  Copyright (C) 2023 DigiPen Institute of Technology. Reproduction or
+  disclosure of this file or its contents without the prior written consent
+  of DigiPen Institute of Technology is prohibited.
+**************************************************************************/
+
 #pragma once
 
 #include "Editor.h"
