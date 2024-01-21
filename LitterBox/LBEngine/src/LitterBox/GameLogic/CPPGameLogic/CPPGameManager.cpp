d8f65d891ac6bd0e2d27beba78f71f2e5516c0af
SmallGuillotine <foodpunch@gmail.com>
2024-01-22 01:32:58 +0800

    Small Grid Change, Tilemap test and Wave Manager WIP

--- LitterBox/LBEngine/src/LitterBox/GameLogic/CPPGameLogic/CPPGameManager.cpp
+++ LitterBox/LBEngine/src/LitterBox/GameLogic/CPPGameLogic/CPPGameManager.cpp
@@ -5,8 +5,30 @@ namespace LB
 {
 	void CPPSGameManager::Start()
 	{
+		//loading the gameobjects with data
+		JSONSerializer::DeserializeFromFile("Mage", *mageEnemy);
+		JSONSerializer::DeserializeFromFile("EnemyChaser1", *chaserEnemy);
+		//then adding it to our pair list
+		EnemyPrefabList.emplace_back(std::make_pair(chaserEnemy, 2));
+		EnemyPrefabList.emplace_back(std::make_pair(mageEnemy, 5));
+		//For the first level we just make it such that it's always 2 melee enemies
+		if (currentWave == 1) 
+		{
+			SpawnCredits = 4;
+		}
 	}
 	void CPPSGameManager::Update()
 	{
+		//We only want the credits to be affected AFTER the first level
+		if (currentWave > 1)
+		{	//Formula => 12/(0.5f + e^(-x/5 + 4))
+			SpawnCredits = 12 / (0.5f + expf(-(currentWave / 5) + 4));
+		}
+
+
+	}
+	void CPPSGameManager::Destroy()
+	{
+		//Should be empty
 	}
 }
--- LitterBox/LBEngine/src/LitterBox/GameLogic/CPPGameLogic/CPPGameManager.h
+++ LitterBox/LBEngine/src/LitterBox/GameLogic/CPPGameLogic/CPPGameManager.h
@@ -1,7 +1,9 @@
 #pragma once
 #include "CPPBehaviour.h"
 #include "LitterBox/Factory/Components.h"
-
+#include "LitterBox/Serialization/AssetManager.h"
+#include <vector>
+#include <utility> //for std::pair
 namespace LB
 {
 	class CPPSGameManager : public CPPBehaviour
@@ -11,12 +13,23 @@ namespace LB
 		void Update() override;
 		void Destroy() override;
 
+		void GenerateWave();
+
 		//Need an array of game objects which will be the enemies to spawn
 		//Assign values to each enemy
 		//Each wave also has a certain value
 		//Spawn enemy and deduct the values
+		GameObject* mPlayer;
+		//Pairs of enemy to value cost for the game manager to spawn
+		std::vector<std::pair<GameObject*, float>> EnemyPrefabList;
 
 	private:
-
+		//Formula made in desmos, curve is a sexy sexy S curve.
+		//Formula => 12/(0.5f + e^(-x/5 + 4))
+		int SpawnCredits{}; //Spawn credits allowed for the game manager 
+		int currentWave{ 1 };	//starting wave is always 1
+		//We try to store references to the enemies
+		GameObject* mageEnemy;
+		GameObject* chaserEnemy;
 	};
 }
