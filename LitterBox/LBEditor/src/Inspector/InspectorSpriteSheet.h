c132f7b81f40790625ea0d6d2dfbd97ff189267f
Hizzykid <112919588+B0B0ChaCha@users.noreply.github.com>
2024-02-06 12:27:19 +0800

    Comments for Inspector SpriteSheet

--- LitterBox/LBEditor/src/Inspector/InspectorSpriteSheet.h
+++ LitterBox/LBEditor/src/Inspector/InspectorSpriteSheet.h
@@ -6,7 +6,8 @@
  \date				11/10/2023
  \brief
 
- This file contains
+ This file contains the class is where it updates the spritesheet, split, 
+ save, etc the spritesheet in the inspector. Declarations.
 
  Copyright (C) 2023 DigiPen Institute of Technology. Reproduction or
  disclosure of this file or its contents without the prior written consent
@@ -22,59 +23,69 @@
 
 namespace LB
 {
+    /*!***********************************************************************
+      \brief
+      InspectorSpriteSheet Class that inherits layer and singleton.
+    *************************************************************************/
 	class InspectorSpriteSheet : public Layer, public Singleton<InspectorSpriteSheet>
 	{
     public:
         /*!***********************************************************************
           \brief
-
+          Initialize function, where it init the textureID, slotID, textureAspect,
+          textureSize
         *************************************************************************/
         void Initialize() override;
 
         /*!***********************************************************************
           \brief
-
+         Where ImGui will be to show on the Inspector, runs the splitting sprite
+         function
         *************************************************************************/
         void UpdateLayer() override;
 
+        /*!***********************************************************************
+          \brief
+         Saving the SpriteSheet, where it serialise the spritesheet
+        *************************************************************************/
         void SaveSpriteSheet();
 
+        /*!***********************************************************************
+          \brief
+         Loads the AssetManager and deserailise the spritesheet
+        *************************************************************************/
         void LoadSpriteSheet(std::string name);
 
+        /*!***********************************************************************
+          \brief
+         Shows Imgui to allow user to manual and auto slice (auto slice for now),
+         user can set the amount of rows and cols for how many sprites they want
+         to split into from the spritesheet
+        *************************************************************************/
         void SpilttingTheSprites();
+
+        /*!***********************************************************************
+          \brief
+         Creating the UV of the spritesheet
+        *************************************************************************/
         void createUV(int rows, int cols);
 
-        ////function for generating appropriate size preview
-        //void calPreviewSize(float width, float height, float previewLimit);
-
-        //inline unsigned int getNumOfTiles() { return tiles.size(); }
-        //inline int getTextureID() { return textureID; }
-        //inline auto getMMUV(int index) { return tiles.at(index); }
 
     private:
-        //ImVec4 m_buttonOffColor{ 0.05f, 0.2f, 0.4f, 1.0f };
-        //ImVec4 m_buttonOnColor{ 0.2f, 0.6f, 1.0f, 1.0f };
 
-        //std::vector< //tiles
-        //    std::pair< //uv
-        //    std::pair<float, float>,  //min x,y
-        //    std::pair<float, float> //max x,y
-        //    >
-        //> tiles{};
+        SpriteSheet m_inspectedSheet; 
 
-        SpriteSheet m_inspectedSheet;
-
-        char m_name[256]{};
-        int m_textureID{ 0 }; // For ImGUI to display
-        int m_sliceX{ 0 }, m_sliceY{ 0 }, m_sliceWidth{ 0 }, m_sliceHeight{ 0 };
-        int m_row{ 0 }, m_col{0};
+        char m_name[256]{}; //
+        int m_textureID{ 0 }; // Getting the texture ID
+        int m_sliceX{ 0 }, m_sliceY{ 0 }, m_sliceWidth{ 0 }, m_sliceHeight{ 0 }; //in update layer, set x and y, width and height
+        int m_row{ 0 }, m_col{0}; //set row and col
 
         int m_spriteX, m_spriteY, m_spriteW, m_spriteH;
 
 
-        int textureID, slotID;
-        float textureAspect;
-        ImVec2 textureSize;
+        int textureID, slotID; //textureID ad slotID
+        float textureAspect; //Aspect of the ratio
+        ImVec2 textureSize; //Texture Size
 	};
 
     extern InspectorSpriteSheet* INSPECTORSPRITESHEET;
