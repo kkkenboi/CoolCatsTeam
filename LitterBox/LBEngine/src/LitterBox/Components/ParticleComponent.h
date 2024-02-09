ee7bcf69577a5ce9f5a16289e4f9bbcf8ca78e21
Ulthyr <113164184+Ulthyr@users.noreply.github.com>
2024-02-09 13:35:14 +0800

    Comments

--- LitterBox/LBEngine/src/LitterBox/Components/ParticleComponent.h
+++ LitterBox/LBEngine/src/LitterBox/Components/ParticleComponent.h
@@ -1,3 +1,21 @@
+/*!************************************************************************
+ \file				ParticleComponent.h
+ \author(s)			Justine Carlo Villa Ilao
+ \par DP email(s):	justine.c@digipen.edu
+ \par Course:		CSD2451A
+ \date				09-02-2024
+ \brief
+  This file contains the ParticleManager class which contains
+  all the declarations of the class' functions. It handles
+  the Particle Pool and the Emitter Pool by updating the
+  positions and stats of Particles that were emitted by an
+  Emitter.
+
+  Copyright (C) 2024 DigiPen Institute of Technology. Reproduction or
+  disclosure of this file or its contents without the prior written consent
+  of DigiPen Institute of Technology is prohibited.
+**************************************************************************/
+
 #pragma once
 #include "Component.h"
 #include "TransformComponent.h"
@@ -17,22 +35,48 @@ namespace LB {
 		// IComponent stuff
 		/*!***********************************************************************
 		\brief
-		Initialises the ParticleManager class by making sure all the indexes for
-		the EmitterPool and the ParticlePool are correct as well as the
-		types of emitters
+		Initialises some of the variables in the CPParticle class as well
+		as adding the newly created CPParticle into the Emitter Pool within
+		the Particle Manager
 		*************************************************************************/
 		void Initialise() override;
-
+		/*!***********************************************************************
+			\brief
+			Updates the CPParticle's position and other stats
+		*************************************************************************/
 		void Update() override;
-
+		/*!***********************************************************************
+			\brief
+			Removes the emitter from the Emitter Pool in the ParticleManager class
+		*************************************************************************/
 		void Destroy() override;
+		/*!***********************************************************************
+			\brief
+			Gets the ComponentTypeID of the CPParticle class
+		*************************************************************************/
 		static ComponentTypeID GetType();
 
+		/*!***********************************************************************
+			\brief
+			Serializes the CPParticle component
+		*************************************************************************/
 		bool Serialize(Value& data, Document::AllocatorType& alloc) override;
+		/*!***********************************************************************
+			\brief
+			Deserializes the CPParticle component
+		*************************************************************************/
 		bool Deserialize(const Value& data) override;
 		// CPParticle stuff
+		/*!***********************************************************************
+			\brief
+			Returns the current emitter type of the CPParticle component as a string
+		*************************************************************************/
 		std::string GetEmitterType();
 
+		/*!***********************************************************************
+			\brief
+			Restarts the lifetime remaining of the CPParticle component
+		*************************************************************************/
 		void RestartLifetime();
 
 	public:
