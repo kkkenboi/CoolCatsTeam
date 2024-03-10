8ba332e5d4de592c8ec278104fec0bfe11411849
JarrettAng <jarrett_ang@hotmail.com>
2024-03-10 10:34:31 +0800

    BrambleBush prefabs + shadows

--- LitterBox/LBEngine/src/LitterBox/Components/RenderComponent.cpp
+++ LitterBox/LBEngine/src/LitterBox/Components/RenderComponent.cpp
@@ -26,6 +26,7 @@ namespace LB
 		data.AddMember("Active", m_active, alloc);
 		data.AddMember("Width", w, alloc);
 		data.AddMember("Height", h, alloc);
+		data.AddMember("ZLayer", z_val, alloc);
 		data.AddMember("Layer", (int)renderer_id, alloc);
 		Value textureName(ASSETMANAGER->GetTextureName(texture).c_str(),alloc);
 		data.AddMember("Texture", textureName, alloc);
@@ -45,6 +46,7 @@ namespace LB
 		bool HasTexture = data.HasMember("Texture");
 		bool HasWidth = data.HasMember("Width");
 		bool HasHeight = data.HasMember("Height");
+		bool HasZLayer = data.HasMember("ZLayer");
 		bool HasLayer = data.HasMember("Layer");
 		bool HasSpriteSheet = data.HasMember("SpriteName");
 		bool HasSpriteIndex = data.HasMember("SpriteIndex");
@@ -53,6 +55,7 @@ namespace LB
 			if (HasActive) m_active = data["Active"].GetBool();
 			if (HasWidth) w = data["Width"].GetFloat();
 			if (HasHeight) h = data["Height"].GetFloat();
+			if (HasZLayer) z_val = data["ZLayer"].GetFloat();
 			if (HasLayer)
 			{
 				Renderer::Renderer_Types type = (Renderer::Renderer_Types)data["Layer"].GetInt();
