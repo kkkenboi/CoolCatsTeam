0bd1b28a136f9003f4fa5cc643fe5f266b61b0a2
Kenji <116390511+kkkenboi@users.noreply.github.com>
2024-03-10 11:08:50 +0800

    HUD Update (5) & return of FPS
    - Fixed a bug where the UI continuously shows if hovering other colliders

--- LitterBox/LBEngine/src/LitterBox/Renderer/ForwardDeclerators.h
+++ LitterBox/LBEngine/src/LitterBox/Renderer/ForwardDeclerators.h
@@ -145,7 +145,7 @@ namespace Renderer {
 	*************************************************************************/
 	struct message {
 		std::string text{"Hello World"};
-		float x{}, y{}, scale{1.0f}, xbound{100.0f}, scaleSaved{};
+		float x{}, y{}, scale{1.0f}, xbound{300.0f}, scaleSaved{};
 		LB::Vec3<float> color{};
 		std::string font_file_name_wo_ext{"ZillaSlab-Regular"};
 
