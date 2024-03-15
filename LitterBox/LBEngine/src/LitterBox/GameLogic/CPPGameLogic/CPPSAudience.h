a88184beb3aaa92991ad396f6a0f80adbdc8e432
JarrettAng <jarrett_ang@hotmail.com>
2024-03-15 13:55:35 +0800

    Audience (2)

---
+++ LitterBox/LBEngine/src/LitterBox/GameLogic/CPPGameLogic/CPPSAudience.h
@@ -0,0 +1,16 @@
+/*!************************************************************************
+ \file				CPPSAudience.h
+ \author(s)			Ang Jiawei Jarrett
+ \par DP email(s):	a.jiaweijarrett@digipen.edu
+ \par Course:       CSD2401A
+ \date				14/03/2024
+ \brief
+
+ This file contains the behaviour of a general audience member
+
+ Copyright (C) 2023 DigiPen Institute of Technology. Reproduction or
+ disclosure of this file or its contents without the prior written consent
+ of DigiPen Institute of Technology is prohibited.
+**************************************************************************/
+
+#pragma once
