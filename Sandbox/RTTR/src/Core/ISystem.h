a0e41e85056e704c2fa62769ea7132bc05faaf3c
JarrettAng <jarrett_ang@hotmail.com>
2024-04-22 12:22:00 +0800

    RTTR system registration (2)

--- Sandbox/RTTR/src/Core/ISystem.h
+++ Sandbox/RTTR/src/Core/ISystem.h
@@ -1,20 +1,27 @@
 
 #pragma once
 
+#include <string>
+
 namespace LB
 {
 	class ISystem
 	{
 	public:
-		virtual int InitializePriority() const { return 0; }
 		virtual void Initialize() = 0;
 
-		virtual int FixedUpdatePriority() const { return 0; }
-		virtual void FixedUpdate() = 0;
-
-		virtual int UpdatePriority() const { return 0; }
-		virtual void Update() = 0;
-
 		virtual void Shutdown() = 0;
+
+		std::string m_Name{ "Unnamed System" };
+	};
+
+	class IFixedUpdateSystem : public ISystem
+	{
+		virtual void FixedUpdate() = 0;
+	};
+
+	class IUpdateSystem : public ISystem
+	{
+		virtual void Update() = 0;
 	};
 }
--- Sandbox/RTTR/src/Engine/Time.h
+++ Sandbox/RTTR/src/Engine/Time.h
@@ -9,11 +9,7 @@ namespace LB
 	class Time : public ISystem
 	{
 	public:
-		int InitializePriority() const override { return 10; }
 		void Initialize() override;
-
-		void FixedUpdate() override { };
-		void Update() override { };
 
 		void Shutdown() override;
 	};
